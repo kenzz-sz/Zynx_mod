79be02660dee355ceba002c4ec8e5e5e739d98ed
kenzz-sz <r4mseiandsei@gmail.com>
2026-04-29 11:24:37 +0700

    update main.js

--- mods/explore/main.js
+++ mods/explore/main.js
@@ -1,18 +1,37 @@
 (function(){
  window.createui = function(){
+    const maint = document.getElementById("app-container")
+    const scene = document.createElement("div");
+    scene.innerHTML = `
+    <div id="scene-explore-mods" class="panel">
+                <div style="display: flex; align-items: center; margin-bottom: 20px;">
+                    <span onclick="App.changeScene('scene-dashboard')" style="color: #0A84FF; font-size: 16px; cursor: pointer; display: flex; align-items: center;">
+                        <span style="font-size: 20px; margin-right: 5px;">‹</span> Back
+                    </span>
+                    <h2 style="margin: 0; font-size: 20px; text-align: center; flex-grow: 1; padding-right: 40px;">EXPLORE - MODS</h2>
+                </div>
+                
+                <div style="background: rgba(0,0,0,0.3); border-radius: 16px; padding: 20px; text-align: center; border: 1px dashed rgba(255,255,255,0.2);">
+                    <p style="opacity: 0.6; font-size: 14px;">[ TITLE ]</p>
+                    <p style="opacity: 0.6; font-size: 12px; font-weight: normal;">Website Subtitle</p>
+                </div>
+            </div>
+    `
+    
+    
   const rc = document.getElementById("scene-dashboard")
-  const cat = document.createElement("div")
-  cat.innerHTML = `<div class="category" id="cat-settings">
-                    <div class="cat-header" onclick="App.toggleCategory('cat-settings')">
-                        <div><span class="cat-icon">👤</span> Preferences</div>
+  const cat = document.createElement("div");
+  cat.innerHTML = `<div class="category" id="Axexport">
+                    <div class="cat-header" onclick="App.toggleCategory('Axexport')">
+                        <div><span class="cat-icon">🌐</span> EXPLORE</div>
                         <div class="cat-chevron">▶</div>
                     </div>
                     <div class="cat-content">
                         <div class="cat-inner">
-                            <!--<div class="feature-item" onclick="App.changeScene('scene-custom')">
-                                <span>Open Editor Workspace</span>
+                            <div class="feature-item" onclick="App.changeScene('scene-explore-mods')">
+                                <span>Mods</span>
                                 <span style="color: #0A84FF;">Open</span>
-                            </div>-->
+                            </div>
                         </div>
                     </div>
                 </div>`;
