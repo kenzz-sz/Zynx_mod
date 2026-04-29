4e13887c55131da0032d1249278b9fcc60eb6d90
kenzz-sz <r4mseiandsei@gmail.com>
2026-04-29 12:19:55 +0700

    update

--- mods/explore/main.js
+++ mods/explore/main.js
@@ -1,5 +1,7 @@
 (function(){
- window.createui = function(){
+    let listmodsexplore = []
+ window.createui = async function(){
+     listmodsexplore = await fh("https://raw.githubusercontent.com/kenzz-sz/Zynx_mod/refs/heads/main/mods/explore/mods.json");
     const maint = document.getElementById("app-container")
     maint.innerHTML += `<div id="scene-explore-mods" class="panel">
                 <div style="display: flex; align-items: center; margin-bottom: 20px;">
@@ -9,10 +11,7 @@
                     <h2 style="margin: 0; font-size: 20px; text-align: center; flex-grow: 1; padding-right: 40px;">EXPLORE - MODS</h2>
                 </div>
                 
-                <div style="background: rgba(0,0,0,0.3); border-radius: 16px; padding: 20px; text-align: center; border: 1px dashed rgba(255,255,255,0.2);">
-                    <p style="opacity: 0.6; font-size: 14px;">[ TITLE ]</p>
-                    <p style="opacity: 0.6; font-size: 12px; font-weight: normal;">Website Subtitle</p>
-                </div>
+                <div id="AXdivexploremods"></div>
             </div>
     `;
   const rc = document.getElementById("scene-dashboard")
@@ -34,5 +33,45 @@
                 rc.appendChild(cat)
                 
  }
+ window.ref = async function(){
+    const mvin = document.getElementById("AXdivexploremods");
+    if (!mvin) return; 
+        mvin.innerHTML = ""; 
+        
+            let res = "";
+            
+            listmodsexplore.forEach(i => {
+                if(!i.name){
+                const idthisvalue = "cat-" + i.directory + "-mymodsvalueid";
+                mvin.innerHTML += `
+                <div class="category" id="${idthisvalue}">
+                    <div class="cat-header" onclick="App.toggleCategory('${idthisvalue}')">
+                        <div><span class="cat-icon">${i.icon || "📦"}</span> ${i.display || i.name  || "-"}</div>
+                        <div class="cat-chevron">▶</div>
+                    </div>
+                    <div class="cat-content">
+                        <div class="cat-inner">
+                            <p style="font-size:12px; opacity:0.6; margin-bottom:10px;">
+                                ${i.descripsion || i.description || 'No description'}
+                            </p>
+                            <div class="feature-item">
+                                <button class="btn-primary" onclick="deleteinstalledmods('${i.directory || null}')" style="margin-top: -10px">
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+            }});
+            if(listmodsexplore.length == 1){
+                mvin.innerHTML = `
+                <div style="background: rgba(0,0,0,0.3); border-radius: 16px; padding: 20px; text-align: center; border: 1px dashed rgba(255,255,255,0.2);">
+                    <p style="opacity: 0.6; font-size: 14px;">[ YOU DONT HAVE ANY MODS ]</p>
+                    <p style="opacity: 0.6; font-size: 12px; font-weight: normal;">Download mods in mods menu.</p>
+                </div>
+            `
+            }
+ }
+ ref()
  createui()
 })()
