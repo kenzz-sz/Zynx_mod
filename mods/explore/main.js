3f5f43c7423b11b3a2e76312223b540e276dbdd9
kenzz-sz <r4mseiandsei@gmail.com>
2026-05-17 13:12:15 +0700

    update main.js

--- mods/explore/main.js
+++ mods/explore/main.js
@@ -55,7 +55,7 @@
                     }
                 }
                 btnentr()
-                ">ENTER</button>
+                ">ENTER</button><br>
                 <div style="background: rgba(0,0,0,0.3); border-radius: 16px; padding: 20px; text-align: center; border: 1px dashed rgba(255,255,255,0.2);">
                     <div id="ddddexplore">-</div>
                 </div>
