7be776288adef30c262d3818ee566b43f9c6fc61
kenzz-sz <r4mseiandsei@gmail.com>
2026-06-07 19:26:27 +0700

    update

--- mods/XZone/main.js
+++ mods/XZone/main.js
@@ -8,6 +8,31 @@ document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/ke
 
 window.createuixzone = async function(){
     const maint = document.getElementById("app-container")
-    
+    maint += `<div id="scene-xzone-main" class="panel">
+                <div style="display: flex; align-items: center; margin-bottom: 20px;">
+                    <span onclick="App.changeScene('scene-dashboard')" style="color: #0A84FF; font-size: 16px; cursor: pointer; display: flex; align-items: center;">
+                        <span style="font-size: 20px; margin-right: 5px;">‹</span> Back
+                    </span>
+                    <h2 style="margin: 0; font-size: 20px; text-align: center; flex-grow: 1; padding-right: 40px;">XZone</h2>
+                </div>
+                
+            </div>`;
+            const rc = document.getElementById("consdash")
+  const cat = document.createElement("div");
+  cat.innerHTML = `<div class="category" id="cat-xzone">
+                    <div class="cat-header" onclick="App.toggleCategory('cat-xzone')">
+                        <div><span class="cat-icon">💍</span> XZone</div>
+                        <div class="cat-chevron">▶</div>
+                    </div>
+                    <div class="cat-content">
+                        <div class="cat-inner">
+                            <div class="feature-item" onclick="App.changeScene('scene-xzone-main');">
+                                <span>Main</span>
+                                <span style="color: pink;">Open</span>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>`;
+                rc.appendChild(cat)
  }
- 
